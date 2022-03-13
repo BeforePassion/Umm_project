bdc040854fed7558b6548e2dce510572db49be3f
BB-Kang <96563366+BB-Kang@users.noreply.github.com>
2022-03-13 15:38:06 +0900

    add test and api

--- recommend/tests.py
+++ recommend/tests.py
@@ -28,7 +28,7 @@ class RecommendModelTestClass(TestCase):
 
         lecture = Lecture.objects.all()
         Recommend.objects.create(
-            user_id=user[0],
+            user=user[0],
             lecture=lecture[0]
         )
 
@@ -36,3 +36,22 @@ class RecommendModelTestClass(TestCase):
 
         assert isinstance(recommend[0], Recommend)
         assert recommend[0].user_id == 1  # Recommend에 들어간 유저가 처음 생성 해놓은 유저가 맞는지
+
+
+class RecommendViewTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_recommend_view(self):
+        # 만든 api가 제대로 routing 되었는지 확인
+        res = self.client.post("/api/recommend/")
+
+        assert res.status_code == 200  # 200: 성공
+
+    def test_wrong_view(self):
+        # 잘못된 api(url)로 연결할 경우
+        res = self.client.post("api/recommend/")
+
+        assert res.status_code == 404  # 404: not found
+
