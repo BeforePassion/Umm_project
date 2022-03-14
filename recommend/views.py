2f169f992837175c961b2c9fb7bae053c70b258e
BB-Kang <96563366+BB-Kang@users.noreply.github.com>
2022-03-14 10:22:19 +0900

    views.py 살짝 수정

--- recommend/views.py
+++ recommend/views.py
@@ -4,8 +4,9 @@ from ninja import Router
 router = Router()
 
 
-@router.post("")  # 이미 앞 url이 api/recommend/ 인 경우에 이 def 작동하는거네
+@router.post("")  # 이미 앞 url이 api/recommend/ 인 경우에 이 def 작동
 def create_recommendations(request):
+
     # request body 추출& 가공 ninja API router것들 작성
     datas = []
     # 추천 ai 모듈 작동 - 문제
@@ -13,4 +14,5 @@ def create_recommendations(request):
 
     # 필요시 result 가공
     presenter = []
-    return "recommendations.html"
+
+    return "recommendations.html"  # html 파일 열면서 ai 모듈 돌려서 나온 결과값들 함께 출력
