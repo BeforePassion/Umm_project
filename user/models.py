bb666168be32b72a75f856dcf5c7978eeeb1d96d
BB-Kang <96563366+BB-Kang@users.noreply.github.com>
2022-03-15 15:45:27 +0900

    user model conflict

--- user/models.py
+++ user/models.py
@@ -3,31 +3,3 @@ from django.db import models
 # Create your models here.
 
 
-class UserModel(models.Model):
-    class Meta:
-        db_table = "users"
-
-    USER_TYPE_CHOICES = (
-        ('Mentor', 'Mentor'),
-        ('Mentee', 'Mentee')
-    )
-
-    EXPERIENCE_CHOICES = (
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-        ('6', '6'),
-        ('7', '7'),
-        ('8', '8'),
-        ('9', '9'),
-        ('10+', '10+')
-    )
-
-    email = models.CharField(max_length=45)
-    username = models.CharField(max_length=45)
-    password = models.CharField(max_length=50)
-    user_type = models.CharField(choices=USER_TYPE_CHOICES, max_length=6)
-    experience = models.CharField(choices=EXPERIENCE_CHOICES, max_length=3)
-    is_deleted = models.BooleanField(default=False)
