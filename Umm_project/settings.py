1b4101d1698343af7e9e5acaf7b1a321b1febbc7
Elijahrong <nhkim5760@gmail.com>
2022-03-25 10:39:23 +0900

    database setting

--- Umm_project/settings.py
+++ Umm_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Umm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sys',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'umm-project.clsjddrfobqa.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
