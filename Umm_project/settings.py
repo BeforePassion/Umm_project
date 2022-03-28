2bbeafc0c37779ab84d99498b214533e704495bd
BB-Kang <96563366+BB-Kang@users.noreply.github.com>
2022-03-28 10:30:02 +0900

    update

--- Umm_project/settings.py
+++ Umm_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'channels',
+    # 'channels',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
--- recommend/views.py
+++ recommend/views.py
@@ -4,32 +4,32 @@ import requests
 router = Router()
 
 
-def make_urllist(page_num, language):
-    urllist = []
+def make_url_list(page_num, language):
+    url_list = []
 
     for i in range(1, page_num + 1):
         url = f'https://www.udemy.com/courses/search/?src=ukw&q={language}'
 
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
 
-        news = requests.get(url, headers=headers)
+        lecture = requests.get(url, headers=headers)
 
-        soup = BeautifulSoup(news.content, 'html.parser')
+        soup = BeautifulSoup(lecture.content, 'html.parser')
 
-        news_list = soup.select('u124-popper-trigger--243 > div.course-card--main-content--2XqiY.course-card--has-price-text--1c0ze > h3 > a')
-        news_list.extend(soup.select('u124-popper-trigger--243 > div.course-card--main-content--2XqiY.course-card--has-price-text--1c0ze > h3 > a'))
-        print(news_list)
-    for line in news_list:
-        urllist.append(line.a.get('href'))
-    return urllist
+        lecture_list = soup.select('u124-popper-trigger--243 > div.course-card--main-content--2XqiY.course-card--has-price-text--1c0ze > h3 > a')
+        lecture_list.extend(soup.select('u124-popper-trigger--243 > div.course-card--main-content--2XqiY.course-card--has-price-text--1c0ze > h3 > a'))
+        print(lecture_list)
+    for line in lecture_list:
+        url_list.append(line.a.get('href'))
+    return url_list
 
 
 @router.post("")  # 이미 앞 url이 api/recommend/ 인 경우에 이 def 작동
 def create_recommendations(request):
 
-    urllist = make_urllist(1, 101, 20220319)
+    url_list = make_url_list(1, 'python')
 
-    print(urllist)
+    print(url_list)
     # request body 추출& 가공 ninja API router것들 작성
     datas = []
     # 추천 ai 모듈 작동 - 문제
