0ec9217777fb83f9769ba2ff730777dff3bd9bbf
Elijahrong <nhkim5760@gmail.com>
2022-05-20 05:55:54 +0900

    setting complete

--- Umm_project/settings.py
+++ Umm_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-from .our_settings import(
+from .secret_settings import(
     MY_DATABASE, Django_Secret_Key
 )
 
